8b75d5a64c6e94d5f4c92ba838637241b77a85ea
LAWAL <haydelawal@gmail.com>
2022-09-15 23:12:38 +0100

    Direct Reply Notification

---
+++ Notification_Direct_Reply/app/src/main/java/com/example/notification_direct_reply/hilt/MyApplication.kt
@@ -0,0 +1,7 @@
+package com.example.notification_direct_reply.hilt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application()
