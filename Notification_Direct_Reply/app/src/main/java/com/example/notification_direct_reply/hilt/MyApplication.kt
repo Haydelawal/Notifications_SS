a8ff96adfd1287c9c3d0d32d058c980d3061756d
Lawal Hayde <81729609+Haydelawal@users.noreply.github.com>
2022-09-19 23:31:27 +0100

    Delete Notification_Direct_Reply directory

--- Notification_Direct_Reply/app/src/main/java/com/example/notification_direct_reply/hilt/MyApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.example.notification_direct_reply.hilt
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MyApplication: Application()
